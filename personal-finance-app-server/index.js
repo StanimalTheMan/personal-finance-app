00e8930345b30ba0788b2aabf6e04218d87980f1
StanimalTheMan <sjc591@nyu.edu>
2023-03-30 19:38:16 -0400

    Use cors and bodyParser middleware

--- personal-finance-app-server/index.js
+++ personal-finance-app-server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/hello", (request, response) => {
   response.json({message: "hello world"});
