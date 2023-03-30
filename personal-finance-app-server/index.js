a9f6eca48e30205d6dfece7a1abc875f69f3b6d6
StanimalTheMan <sjc591@nyu.edu>
2023-03-29 23:37:49 -0400

    add cors and body-parser, prev. commit

--- personal-finance-app-server/index.js
+++ personal-finance-app-server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/hello", (request, response) => {
+  response.json({message: "hello world"});
+}); 
+
 app.listen(8000, () => {
   console.log("server has started");
 });
