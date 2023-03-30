3f5d5bdb560e95e041bc73a3f033c0cf5c5a7140
StanimalTheMan <sjc591@nyu.edu>
2023-03-30 19:49:26 -0400

    add changes

--- personal-finance-app-client/src/App.jsx
+++ personal-finance-app-client/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
+  useEffect(() => {
+    async function fetch() {
+      
+    }
+  }, []);
 
   return (
     <span>Hello World!</span>
