57179a3aa7e330e07c5a3d260f2153abb8305606
StanimalTheMan <sjc591@nyu.edu>
2023-03-30 20:12:28 -0400

    use node plaid npm package, sandbox env for now

--- personal-finance-app-server/index.js
+++ personal-finance-app-server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': CLIENT_ID,
+      'PLAID-SECRET': SECRET,
+    },
+  },
+});
+
+const client = new PlaidApi(configuration);
 
 const app = express();
 app.use(cors());
